df746d9cfc0c52dd5f2027ac1026346d7aa8aec6
antonio-bit-1109 <143196020+antonio-bit-1109@users.noreply.github.com>
2024-01-30 17:48:41 +0100

    gg

--- Esercizio 2 c sharp/Program.cs
+++ Esercizio 2 c sharp/Program.cs
@@ -6,6 +6,7 @@ namespace Esercizio_2_c_sharp
     {
         static void Main(string[] args)
         {
+            // istanza standard
             Persona persona1 = new Persona("Giancarlo", "Fumagalli", 99);
             Console.WriteLine(persona1.GetNome());
             Console.WriteLine(persona1.GetCognome());
@@ -16,13 +17,12 @@ namespace Esercizio_2_c_sharp
 
 
 
-
+            // input utente
             Console.WriteLine("inserisci il tuo Nome:");
             string nomeAltro = Console.ReadLine();
 
             Console.WriteLine("inserisci il tuo Cognome:");
             string cognomeAltro = Console.ReadLine();
-
 
             Console.WriteLine("Inserisci la tua età:");
             int inputetà = Convert.ToInt32(Console.ReadLine());
@@ -34,6 +34,7 @@ namespace Esercizio_2_c_sharp
             {
                 Console.WriteLine("Errore: Devi inserire un numero per l'età. Riprova:");
             }*/
+
 
             Persona antonio = new Persona(nomeAltro, cognomeAltro, inputetà);
             antonio.GetNome();
