f019cea0ed478e26f37681d495508b387cf1c4aa
antonio-bit-1109 <143196020+antonio-bit-1109@users.noreply.github.com>
2024-01-30 14:20:35 +0100

    Aggiungere i file di progetto.

---
+++ Esercizio 2 c sharp/Persona.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esercizio_2_c_sharp
+{
+    internal class Persona
+    {
+        public string Nome;
+        public string Cognome;
+        public int Eta; 
+
+
+        public Persona(string nome, string cognome, int eta)
+        {
+            this.Nome = nome;
+            this.Cognome = cognome;
+            this.Eta = eta;
+        }
+
+        public string GetNome() 
+        {
+            return $"il mio nome è {this.Nome}"; 
+        }
+
+        public string GetCognome()
+        { return $"il mio cognome è {this.Cognome}"; }
+
+        public void getDettagli() 
+        {
+            Console.WriteLine($"questa persona si chiama {this.Nome}, di cognome fa {this.Cognome} e ha un età di {this.Eta} anni ");
+        }
+    }
+}
---
+++ Esercizio 2 c sharp/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esercizio_2_c_sharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona persona1 = new Persona("Giancarlo", "Fumagalli", 99);
+             Console.WriteLine( persona1.GetNome());
+             Console.WriteLine( persona1.GetCognome());
+             persona1.getDettagli();
+
+            Console.WriteLine("\n"); 
+
+
+            Console.WriteLine("inserisci il tuo Nome:");
+            string nomeAltro = Console.ReadLine();
+
+            Console.WriteLine("inserisci il tuo Cognome:");
+            string cognomeAltro = Console.ReadLine();
+
+
+            Console.WriteLine("Inserisci la tua età:");
+            // int inputetà = Convert.ToInt32(Console.ReadLine());
+            int inputetà; 
+            while (!int.TryParse(Console.ReadLine() , out inputetà))
+            {
+                Console.WriteLine("Errore: Devi inserire un numero per l'età. Riprova:");
+            }
+
+            Persona antonio = new Persona(nomeAltro , cognomeAltro , inputetà);
+            antonio.GetNome();
+            antonio.GetCognome();
+            antonio.getDettagli(); 
+
+            Console.ReadLine(); 
+        }
+    }
+}
