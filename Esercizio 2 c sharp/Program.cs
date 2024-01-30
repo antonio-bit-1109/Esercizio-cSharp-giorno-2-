3063b31fd76b82ba2382f4e319185cb23a303d7c
antonio-bit-1109 <143196020+antonio-bit-1109@users.noreply.github.com>
2024-01-30 16:40:39 +0100

    fix

--- Esercizio 2 c sharp/Program.cs
+++ Esercizio 2 c sharp/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Esercizio_2_c_sharp
 {
@@ -11,11 +7,14 @@ namespace Esercizio_2_c_sharp
         static void Main(string[] args)
         {
             Persona persona1 = new Persona("Giancarlo", "Fumagalli", 99);
-             Console.WriteLine( persona1.GetNome());
-             Console.WriteLine( persona1.GetCognome());
-             persona1.getDettagli();
+            Console.WriteLine(persona1.GetNome());
+            Console.WriteLine(persona1.GetCognome());
+            persona1.getDettagli();
 
-            Console.WriteLine("\n"); 
+            Console.WriteLine("\n");
+
+
+
 
 
             Console.WriteLine("inserisci il tuo Nome:");
@@ -26,19 +25,22 @@ namespace Esercizio_2_c_sharp
 
 
             Console.WriteLine("Inserisci la tua età:");
-            // int inputetà = Convert.ToInt32(Console.ReadLine());
-            int inputetà; 
-            while (!int.TryParse(Console.ReadLine() , out inputetà))
+            int inputetà = Convert.ToInt32(Console.ReadLine());
+
+
+            //int inputetà; 
+
+            /*while (!int.TryParse(Console.ReadLine() , out inputetà))
             {
                 Console.WriteLine("Errore: Devi inserire un numero per l'età. Riprova:");
-            }
+            }*/
 
-            Persona antonio = new Persona(nomeAltro , cognomeAltro , inputetà);
+            Persona antonio = new Persona(nomeAltro, cognomeAltro, inputetà);
             antonio.GetNome();
             antonio.GetCognome();
-            antonio.getDettagli(); 
+            antonio.getDettagli();
 
-            Console.ReadLine(); 
+            Console.ReadLine();
         }
     }
 }
