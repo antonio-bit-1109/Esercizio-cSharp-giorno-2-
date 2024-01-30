8dfaa09d7874978e27ce1c23aae4c914e3b03de7
antonio-bit-1109 <143196020+antonio-bit-1109@users.noreply.github.com>
2024-01-30 17:11:57 +0100

    fix codice

--- Esercizio 2 c sharp/Persona.cs
+++ Esercizio 2 c sharp/Persona.cs
@@ -4,11 +4,13 @@ namespace Esercizio_2_c_sharp
 {
     internal class Persona
     {
+        // definizione proprietà classe
         public string Nome { get; set; }
         public string Cognome { get; set; }
         public int Eta { get; set; }
 
 
+        // definisco costruttore
         public Persona(string nome, string cognome, int eta)
         {
             this.Nome = nome;
@@ -16,6 +18,7 @@ namespace Esercizio_2_c_sharp
             this.Eta = eta;
         }
 
+        // metodi return 
         public string GetNome()
         {
             return $"il mio nome è {this.Nome}";
@@ -27,6 +30,7 @@ namespace Esercizio_2_c_sharp
             return $"il mio cognome è {this.Cognome}";
         }
 
+        // procedura void 
         public void getDettagli()
         {
             Console.WriteLine($"questa persona si chiama {this.Nome}, di cognome fa {this.Cognome} e ha un età di {this.Eta} anni ");
